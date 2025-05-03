4b8c2d7f3f3350046924a70a831c787e63292be0
Sean <shperry@ucsd.edu>
2025-05-02 17:15:11 -0700

    feat: add more of the pipeline and remove pycache

--- pyha_analyzer/extractors/birdset.py
+++ pyha_analyzer/extractors/birdset.py
@@ -8,5 +8,9 @@ class Birdset(DefaultExtractor):
 
     def __call__(self, region):
         ds = load_dataset("DBD-research-group/BirdSet", region, trust_remote_code=True)
-        #TODO Clean Features
-        return AudioDataset(ds)
+        xc_ds = ds["train"].train_test_split(test_size=0.2, stratify_by_column="ebird_code")
+        return AudioDataset({
+            "train": xc_ds["train"],
+            "valid": xc_ds["test"],
+            "test": ds["test_5s"]
+        }, f"{self.get_provendence()}-{region}")
