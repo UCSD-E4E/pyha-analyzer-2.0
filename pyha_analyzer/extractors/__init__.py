45aefd5eaaca14b6468699decd1f124076463ec1
AJajodia <jajanu129@gmail.com>
2025-07-09 15:48:06 -0700

    feat: added multi_coral.py for extraction of all coral reef datasets as a single dataset

--- pyha_analyzer/extractors/__init__.py
+++ pyha_analyzer/extractors/__init__.py
@@ -1,3 +1,4 @@
 from .birdset import Birdset as Birdset
 from .coralreef import CoralReef as CoralReef
 from .peru132 import Peru132Extractor as Peru132Extractor
+from .multi_coral import MultiCoralReef
